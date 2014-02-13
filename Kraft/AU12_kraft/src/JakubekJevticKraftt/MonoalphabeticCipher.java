c027d7d376f1a2540863afb111494431871847c2
Kraft <mkraft01@student.tgm.ac.at>
2014-02-13 19:53:46 +0100

    Mono Substitution Update

--- Kraft/AU12_kraft/src/JakubekJevticKraftt/MonoalphabeticCipher.java
+++ Kraft/AU12_kraft/src/JakubekJevticKraftt/MonoalphabeticCipher.java
@@ -58,4 +58,10 @@ public class MonoalphabeticCipher implements Cipher {
 		return String.valueOf(txt);
 
 	}
+	public static void main(String[] args) {
+		Cipher c = new SubstitutionCipher("cdefghijklmnopqrstuvwxyzab");
+		String en = c.encrypt("Test");
+		System.out.println(en);
+		System.out.println(c.decrypt(en));
+	}
 }
--- Kraft/AU12_kraft/src/JakubekJevticKraftt/SubstitutionCipher.java
+++ Kraft/AU12_kraft/src/JakubekJevticKraftt/SubstitutionCipher.java
@@ -9,11 +9,4 @@ public class SubstitutionCipher extends MonoalphabeticCipher {
 	public void setSecretAlphabet(String secretAlphabet) {
 		super.setSecretAlphabet(secretAlphabet);
 	}
-	
-	public static void main(String[] args) {
-		Cipher c = new SubstitutionCipher("cdefghijklmnopqrstuvwxyzab");
-		String en = c.encrypt("Test");
-		System.out.println(en);
-		System.out.println(c.decrypt(en));
-	}
 }
