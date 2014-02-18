13b5405ca8c8c6cdca169d5802739d474de7590f
bobanjevtic-tgm <j.adidas@hotmail.com>
2014-02-18 21:01:51 +0100

    Aktuelle Version von A12

---
+++ Jevtic/A12/src/JakubekJevticKraftt/KeywordCipher.java
@@ -0,0 +1,41 @@
+package JakubekJevticKraftt;
+/**
+ * 
+ * @author Max
+ * @version 1.0
+ */
+public class KeywordCipher extends MonoalphabeticCipher {
+
+	/**
+	 * 
+	 * @param keyword setzen
+	 */
+	public KeywordCipher(String keyword) {
+		this.setKeyword(keyword);
+	}
+
+	/**
+	 * 
+	 * @param keyword zum hinzufügen des secret alphabet
+	 */
+	public void setKeyword(String keyword) {
+		String abc = getSecretAlphabet().toLowerCase();
+		char key[] = keyword.toLowerCase().toCharArray();
+
+		for (int i=0; i<key.length; i++){
+			if(abc.contains(""+key[i])){
+				abc=abc.replaceAll("" + key[i],"");
+			}
+		}
+
+		String secret = String.valueOf(key) + abc;
+
+		this.setSecretAlphabet(secret);
+	}
+	public static void main(String[]args){
+		KeywordCipher sc=new KeywordCipher("dsg");
+		String en = sc.encrypt("sdx");
+		System.out.println(en);
+		System.out.println(sc.decrypt(en));
+	}
+}
